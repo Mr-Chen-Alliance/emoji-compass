b3134975360609e759576763e8a8cec9a01f2d1d
Jimmi Chen <gchen20@u.rochester.edu>
2018-08-28 16:40:49 -0400

    spin the emoji

--- client/compass.js
+++ client/compass.js
@@ -72,69 +72,125 @@
 
 
 import React, { Component } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Platform, Text, View, StyleSheet, Animated, Easing, Dimensions } from 'react-native';
 import { Constants, Location, Permissions } from 'expo';
 
 export default class compass extends Component {
-  state = {
-    // location: null,
-    errorMessage: null,
-    heading: null
-  };
 
-  componentWillMount() {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
-    } else {
-      this._getLocationAsync();
-    }
-  }
-
-  _getLocationAsync = async () => {
-    // Checking device location permissions
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied',
-      });
-    }
-    else
-    {
-      Expo.Location.watchHeadingAsync((obj) => {
-        let heading = obj.magHeading;
-        this.setState({heading: heading})
-      })
-    }
-  };
-  render() {
-    let text = 'Waiting..';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.heading) {
-      text = JSON.stringify(this.state.heading);
+    static navigationOptions = {
+        header: null
     }
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>{text}</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.spinValue = new Animated.Value(0);
+        this.state = {
+            // location: null,
+            errorMessage: null,
+            heading: null
+        };
+    }
+
+    componentWillMount() {
+        if (Platform.OS === 'android' && !Constants.isDevice) {
+            this.setState({
+                errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+            });
+        } else {
+            this._getLocationAsync();
+        }
+    }
+
+    componentWillUpdate() {
+        this.spin()
+    }
+
+    spin() {
+        let start = JSON.stringify(this.spinValue);
+        let heading = Math.round(this.state.heading);
+
+        let rot = +start;
+        let rotM = rot % 360;
+
+        if (rotM < 180 && (heading > (rotM + 180)))
+            rot -= 360;
+        if (rotM >= 180 && (heading <= (rotM - 180)))
+            rot += 360
+
+        rot += (heading - rotM)
+
+        Animated.timing(
+            this.spinValue,
+            {
+                toValue: rot,
+                duration: 300,
+                easing: Easing.easeInOut
+            }
+        ).start()
+    }
+
+    _getLocationAsync = async () => {
+        // Checking device location permissions
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                errorMessage: 'Permission to access location was denied',
+            });
+        }
+        else {
+            Expo.Location.watchHeadingAsync((obj) => {
+                let heading = obj.magHeading;
+                this.setState({ heading: heading })
+            })
+        }
+    };
+    render() {
+        let text = 'Waiting...';
+        const emoji = this.props.navigation.state.params.emoji;
+        if (this.state.errorMessage) {
+            text = this.state.errorMessage;
+        } else if (this.state.heading) {
+            text = JSON.stringify(this.state.heading);
+        }
+
+        const spin = this.spinValue.interpolate({
+            inputRange: [0, 360],
+            outputRange: ['-0deg', '-360deg']
+        })
+
+        text = Math.round(JSON.stringify(this.spinValue))
+
+        if (text < 0)
+            text += 360
+        if (text > 360)
+            text -= 360
+
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>{text + '°'}</Text>
+                <Animated.Text
+                    style={{
+                        transform: [{ rotate: spin }],
+                        fontSize: 160
+                    }} >
+                    {emoji}
+                </Animated.Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    textAlign: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        textAlign: 'center',
+    }
 });
